78d27d4a7a6fef56dfcb93c19001f5a4ad51e38f
alex <astac1@umbc.edu>
2014-11-09 20:07:40 -0500

    Print length of each line on stdin.

---
+++ bin/len.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""usage: cat lines | len"""
+import sys
+
+if __name__ == '__main__':
+    try:
+        for line in sys.stdin.readlines():
+            print len(line)
+    except IOError:  # problem in bash?
+        sys.exit(1)
+    except KeyboardInterrupt:
+        sys.exit(1)
