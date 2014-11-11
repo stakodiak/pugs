cb78d32caab18154881ba05b66ffef3db295aec4
Alexander Stachowiak <alexander@computer.org>
2014-11-11 12:54:13 -0500

    Convert stream of text to rot13.

---
+++ bin/rot13.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""usage: cat text | rot13"""
+import sys
+
+if __name__ == '__main__':
+    try:
+        for line in sys.stdin.readlines():
+            sys.stdout.write(line.encode('rot13'))
+    except IOError:  # problem in bash?
+        sys.exit(1)
+    except KeyboardInterrupt:
+        sys.exit(1)
