4338db6690861c5f57297f9d33d8ff8d01cbc4ee
Alex Stachowiak <alexander@computer.org>
2017-04-30 00:52:51 -0700

    Support "-command" suffix in filenames.
    This avoids any conflicts with imported module names.

--- fetch.py
+++ fetch.py
@@ -5,47 +5,50 @@ import textwrap
 
 def main():
     cwd = os.getcwd()
-    target_dir = '/usr/local/pugs'
-    script_dir = 'barks'
+    target_dir = "/usr/local/pugs"
+    script_dir = "barks"
 
     # create home (dog house?) for pugs scripts
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
-        print "created target (%s)" % target_dir
+        print("created target ({})".format(target_dir))
 
     # make sure pugs is in path
-    if target_dir not in os.environ['PATH']:
+    if target_dir not in os.environ["PATH"]:
         path_script = """
         # pugs utilities
         export PATH=%s:$PATH
         """ % target_dir
-        print "Warning:", target_dir, "not in path"
-        print "Please add to bash config:"
-        print textwrap.dedent(path_script)
+        print("Warning:", target_dir, "not in path")
+        print("Please add to bash config:")
+        print(textwrap.dedent(path_script))
 
     # link to all pugs scripts
     script_names = os.listdir(script_dir)
-    print "Fetched", len(script_names), "scripts."
+    print("added:")
     for i, script_name in enumerate(script_names):
-        if script_name.startswith('.'):
+        link_name = script_name
+        if link_name.startswith("."):
             continue
-        link_name = script_name.rstrip('.py')
+        suffixes = ["-command.py", ".py"]
+        for suffix in suffixes:
+            if link_name.endswith(suffix):
+                link_name = link_name[:-1 * len(suffix)]
         source = os.path.join(cwd, script_dir, script_name)
         target = os.path.join(target_dir, link_name)
-        print i + 1, '-', link_name,
+        print("'{}' -> {}".format(link_name, source))
         try:
             os.symlink(source, target)
             os.chmod(target, 0755)  # user can write, not user can r/x
-            print "(added)"
         except OSError as e:
             if e.errno == 17:
-                print "(exists)"
+                continue
             elif e.errno == 13:
-                print "Can't write to %s!" % target_dir
+                print("Can't write to {}!".format(target_dir))
                 raise e
             else:  # pug down, pug down
                 raise e
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
