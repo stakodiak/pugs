6f2b96febe892e7920667e377966dd203c383889
Alex Stachowiak <alexander@computer.org>
2017-06-21 00:57:06 -0400

    selector: extract text from HTML using CSS selectors

---
+++ barks/selector.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""usage: selector <CSS selector (.btn etc.)>
+
+accepts html through stdin"""
+import getopt
+import sys
+
+from bs4 import BeautifulSoup
+
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+        inf = sys.stdin
+    except getopt.GetoptError:
+        print __doc__
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print __doc__
+            sys.exit()
+    try:
+        soup = BeautifulSoup(inf)
+        for selector in args:
+            for e in soup.select(selector):
+                if len(e) > 1:
+                    print unicode(e)
+                else:
+                    print e.get_text()
+    except IndexError:
+        print __doc__
+        sys.exit(1)
