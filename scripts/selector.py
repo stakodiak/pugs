5db4e8943296e2e0b88055b8c52b43d407f5b4e9
Alex Stachowiak <alexander@computer.org>
2021-08-31 21:40:18 -0400

    selector: Update to allow attribute value extraction

--- scripts/selector.py
+++ scripts/selector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 """usage: selector <CSS selector (.btn etc.)>
 
 accepts html through stdin"""
@@ -15,16 +15,31 @@ if __name__ == '__main__':
     except getopt.GetoptError:
         print(__doc__)
         sys.exit(1)
+
+    # settings for app runtime options
+    config = {
+        "attr_to_print": [],
+        "print_in_html": False,
+    }
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print(__doc__)
             sys.exit()
+        elif opt in ("--html"):
+            config["print_in_html"] = True
+            print("prettt")
+        elif opt in ("-a", "--attr"):
+            config["attr_to_print"].append(arg)
     try:
         soup = BeautifulSoup(inf, "html.parser")
         for selector in args:
             for e in soup.select(selector):
-                if len(e) > 1:
-                    print(e)
+                if "--html" in sys.argv:
+                    print(e.prettify())
+                # e.g. cat source.html | selector img --attr src
+                elif config["attr_to_print"]:
+                    for arg in config["attr_to_print"]:
+                        print(e[arg])
                 else:
                     print(e.get_text())
     except IndexError:
