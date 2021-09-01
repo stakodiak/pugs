b1af94332a1aadd20b21a43b20a5ee6b816d24d4
Alex Stachowiak <alexander@computer.org>
2021-08-31 21:48:11 -0400

    endswith: Print all lines which end with given arg
    Example:
    $ echo "1\nfish" | endswith fish
    fish

--- scripts/endswith.py
+++ scripts/endswith.py
@@ -4,12 +4,13 @@ import sys
 
 if __name__ == '__main__':
     try:
+        # Print all lines whose contents end with given arg
         key = sys.argv[1]
         for line in sys.stdin.readlines():
             if line.rstrip().endswith(key):
                 sys.stdout.write(line)
     except IndexError:
-        print __doc__
+        print(__doc__)
         sys.exit(0)
     except IOError:
         sys.exit(1)
