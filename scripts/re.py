65c981cc9cba4a8fc0b44399c129abe599aaa794
Alex Stachowiak <alexander@computer.org>
2021-08-31 22:00:36 -0400

    re: CLI regex captures and replacements
    Usage examples:
    
    Simple regex-based substitions:
    cat in.html | re "&quot;" "'" > out.html
    
    Simple regex capture and extraction:
    cat ~/.zsh_history| re -c "\d{10}" | u2d | wc -l

--- scripts/re.py
+++ scripts/re.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
-"""usage: re regex [replacement]"""
+"""usage: cat file | re regex [replacement]
+
+Simple regex-based substitions:
+cat in.html | re "&quot;" "'" > out.html
+
+Simple regex capture and extraction:
+cat ~/.zsh_history| re -c "\d{10}" | u2d | wc -l
+"""
 import getopt
 import re
 import sys
@@ -12,11 +19,11 @@ if __name__ == '__main__':
                                   ["help", "capture"])
         inf = sys.stdin
     except getopt.GetoptError:
-        print __doc__
+        print(__doc__)
         sys.exit(1)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print __doc__
+            print(__doc__)
             sys.exit()
         elif opt in ("-c", "--capture"):
             for line in inf:
@@ -24,11 +31,11 @@ if __name__ == '__main__':
                     matches = re.findall(pattern,
                                          line)
                     for match in matches:
-                        print ''.join(match)
+                        print(''.join(match))
             sys.exit()
     try:
         replacement = args[1] if len(args) >= 2 else ''
-        print re.sub(args[0], replacement, inf.read()),
+        print(re.sub(args[0], replacement, inf.read()),)
     except IndexError:
-        print __doc__
+        print(__doc__)
         sys.exit(1)
